5b19e534e1b96658e97e7891c2d5bda3c74ab815
Mário Fernandes <mgustavofernandes@gmail.com>
2017-03-07 15:43:49 +0000

    update project startup

---
+++ src/Projeto1_Cal.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Projeto1_Cal.cpp
+// Author      : Catarina Ramos, Inês Gomes and Mário Fernandes
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
