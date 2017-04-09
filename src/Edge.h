8ebe3579e428368832302353351a2ca86abef7fb
Mário Fernandes <mgustavofernandes@gmail.com>
2017-04-09 15:08:49 +0100

    format + clean out useless code

--- src/Edge.h
+++ src/Edge.h
@@ -8,58 +8,51 @@
 #ifndef EDGE_H_
 #define EDGE_H_
 
+template<class T> class Vertex;
+template<class T> class Graph;
 
-template <class T> class Vertex;
-template <class T> class Graph;
-
-template <class T>
+template<class T>
 class Edge {
 private:
 	Vertex<T> * dest;
 	Vertex<T> * orig;
 	double weight;
-	double flow;
 public:
-	Edge(Vertex<T> *d, double w, double f=0);
+	Edge(Vertex<T> *d, double w);
 	double getFlow() const;
 	double getWeight() const;
 	Vertex<T> *getDest() const;
 	bool operator<(const Edge<T> &other) const;
 
-	friend class Graph<T>;
-	friend class Vertex<T>;
+	friend class Graph<T> ;
+	friend class Vertex<T> ;
 };
 
-template <class T>
-Edge<T>::Edge(Vertex<T> *d, double w, double f): dest(d), weight(w), flow(f){}
-
-template <class T>
-double Edge<T>::getFlow() const {
-	return flow;
+template<class T>
+Edge<T>::Edge(Vertex<T> *d, double w) :
+		dest(d), orig(NULL), weight(w) {
 }
 
-template <class T>
+template<class T>
 double Edge<T>::getWeight() const {
 	return weight;
 }
 
-template <class T>
+template<class T>
 Vertex<T>* Edge<T>::getDest() const {
 	return dest;
 }
 
-template <class T>
+template<class T>
 bool Edge<T>::operator<(const Edge<T> &other) const {
 	return this->weight < other.weight;
 }
 
-template <class T>
+template<class T>
 struct edge_greater_than {
-    bool operator()(Edge<T> a, Edge<T>  b) const {
-        return a.getWeight() > b.getWeight();
-    }
+	bool operator()(Edge<T> a, Edge<T> b) const {
+		return a.getWeight() > b.getWeight();
+	}
 };
-
-
 
 #endif /* EDGE_H_ */
--- src/Vertex.h
+++ src/Vertex.h
@@ -11,129 +11,107 @@
 #include <vector>
 #include <cstddef>
 
-template <class T> class Edge;
-template <class T> class Graph;
+template<class T> class Edge;
+template<class T> class Graph;
 using namespace std;
-template <class T>
+template<class T>
 class Vertex {
 private:
 	T info;	//location
-	vector<Edge<T>  > adj;
+	vector<Edge<T> > adj;
 	bool visited;
 	bool processing;
 	int indegree;
 	double dist;
-	int set;
+	Vertex* path;
 public:
 	Vertex(T in);
-	friend class Graph<T>;
-
-	void addEdge(Vertex<T> *dest, double w);
-	void addEdge(Vertex<T> *dest, double w, double f);
-	bool removeEdgeTo(Vertex<T> *d);
+	friend class Graph<T> ;
 
 	T getInfo() const;
-	void setInfo(T info);
-
-	int getDist() const;
-	int getIndegree() const;
 	vector<Edge<T> > getAdj() const;
+	bool getVisited() const {
+		return this->visited;
+	}
+	int getIndegree() const;
+	int getDist() const;
 	Vertex<T>* getPath() const;
 
+	void setInfo(T info);
+
+	void addEdge(Vertex<T> *dest, double w);
+	bool removeEdgeTo(Vertex<T> *d);
+
 	bool operator<(const Vertex<T> vertex);
-	bool operator==(const Vertex<T> vertex){return this->info == vertex.getInfo();}
-
-	Vertex* path;
-
-	void updateEdgeFlow(unsigned int index, float f);
-	bool getVisited(){return this->visited;}
+	bool operator==(const Vertex<T> vertex) {
+		return this->info == vertex.getInfo();
+	}
 };
 
-template <class T>
+template<class T>
 struct vertex_greater_than {
-    bool operator()(Vertex<T> * a, Vertex<T> * b) const {
-        return a->getDist() > b->getDist();
-    }
+	bool operator()(Vertex<T> * a, Vertex<T> * b) const {
+		return a->getDist() > b->getDist();
+	}
 };
 
-
-template <class T>
+template<class T>
 bool Vertex<T>::removeEdgeTo(Vertex<T> *d) {
 	d->indegree--; //adicionado do exercicio 5
-	typename vector<Edge<T> >::iterator it= adj.begin();
-	typename vector<Edge<T> >::iterator ite= adj.end();
-	while (it!=ite) {
+	typename vector<Edge<T> >::iterator it = adj.begin();
+	typename vector<Edge<T> >::iterator ite = adj.end();
+	while (it != ite) {
 		if (it->dest == d) {
 			adj.erase(it);
 			return true;
-		}
-		else it++;
+		} else
+			it++;
 	}
 	return false;
 }
 
 //atualizado pelo exercício 5
-template <class T>
-Vertex<T>::Vertex(T in): info(in), visited(false), processing(false), indegree(0), dist(0) {
+template<class T>
+Vertex<T>::Vertex(T in) :
+		info(in), visited(false), processing(false), indegree(0), dist(0) {
 	path = NULL;
 }
 
-
-template <class T>
+template<class T>
 void Vertex<T>::addEdge(Vertex<T> *dest, double w) {
-	Edge<T> edgeD(dest,w);
+	Edge<T> edgeD(dest, w);
 	edgeD.orig = this;
 	adj.push_back(edgeD);
 }
 
-template <class T>
-void Vertex<T>::addEdge(Vertex<T> *dest, double w, double f)
-{
-	Edge<T> edgeD(dest, w, f);
-	edgeD.orig = this;
-	adj.push_back(edgeD);
-}
-
-
-template <class T>
+template<class T>
 T Vertex<T>::getInfo() const {
 	return this->info;
 }
 
-template <class T>
+template<class T>
 int Vertex<T>::getDist() const {
 	return this->dist;
 }
 
-template <class T>
+template<class T>
 vector<Edge<T> > Vertex<T>::getAdj() const {
 	return this->adj;
 }
 
-template <class T>
+template<class T>
 Vertex<T>* Vertex<T>::getPath() const {
 	return this->path;
 }
 
-
-template <class T>
+template<class T>
 void Vertex<T>::setInfo(T info) {
 	this->info = info;
 }
 
-template <class T>
+template<class T>
 int Vertex<T>::getIndegree() const {
 	return this->indegree;
 }
-
-template <class T>
-void Vertex<T>::updateEdgeFlow(unsigned int index, float f)
-{
-	if (index >= adj.size())
-		return;
-	adj[index].flow = f;
-}
-
-
 
 #endif /* VERTEX_H_ */
